d404019549da3cdac68f49e3518f87a8f5a60cf8
remludar <jason.ewton@gmail.com>
2015-10-16 18:04:17 -0500

    Initial Shaders and VBOs

---
+++ BasicTexture/fs.glsl
@@ -0,0 +1,8 @@
+﻿#version 330
+
+in vec4 Color;
+out vec4 OutputColor;
+void main()
+{
+	OutputColor = Color;
+}
---
+++ BasicTexture/vs.glsl
@@ -0,0 +1,10 @@
+﻿#version 330
+in vec4 position;
+in vec4 color;
+out vec4 Color;
+
+void main()
+{
+	gl_Position = position;
+	Color = color;
+}
