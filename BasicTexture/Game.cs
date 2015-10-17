a074c0710a42ecdf291f06c124b340fc68467393
remludar <jason.ewton@gmail.com>
2015-10-17 09:24:42 -0500

    Successful single quad with a texture!

--- BasicTexture/Game.cs
+++ BasicTexture/Game.cs
@@ -16,8 +16,8 @@ namespace BasicTexture
     class Game : GameWindow
     {
         float[] vertData;
-        int shaderProgramID, vertShaderID, fragShaderID;
-        int positionAttrib, colorAttrib;
+        int shaderProgramID, vertShaderID, fragShaderID, textureID;
+        int positionAttrib, colorAttrib, textureAttrib;
         int vertexVBO;
 
         protected override void OnLoad(EventArgs e)
@@ -28,6 +28,7 @@ namespace BasicTexture
             
             _LoadData();
             _LoadShaders();
+            _LoadTexture();
             _LoadBuffers();
         }
 
@@ -59,14 +60,14 @@ namespace BasicTexture
         private void _LoadData()
         {
             vertData = new float[]{
-                    //Position            //Color
-                -0.5f, -0.5f, 0.0f,     0.3f, 0.0f, 0.0f, 1.0f,
-                -0.5f, +0.5f, 0.0f,     0.6f, 0.0f, 0.0f, 1.0f,
-                +0.5f, +0.5f, 0.0f,     0.9f, 0.0f, 0.0f, 1.0f,
+                    //Position                  //Color                //UV
+                -0.5f, -0.5f, 0.0f,     0.3f, 0.0f, 0.0f, 1.0f,     1.0f, 1.0f,
+                -0.5f, +0.5f, 0.0f,     0.6f, 0.0f, 0.0f, 1.0f,     1.0f ,0.0f,
+                +0.5f, +0.5f, 0.0f,     0.9f, 0.0f, 0.0f, 1.0f,     0.0f, 0.0f,
 
-                +0.5f, +0.5f, 0.0f,     0.0f, 0.9f, 0.0f, 1.0f,
-                +0.5f, -0.5f, 0.0f,     0.0f, 0.6f, 0.0f, 1.0f,
-                -0.5f, -0.5f, 0.0f,     0.0f, 0.3f, 0.0f, 1.0f
+                +0.5f, +0.5f, 0.0f,     0.0f, 0.9f, 0.0f, 1.0f,     0.0f, 0.0f,
+                +0.5f, -0.5f, 0.0f,     0.0f, 0.6f, 0.0f, 1.0f,     0.0f, 1.0f,
+                -0.5f, -0.5f, 0.0f,     0.0f, 0.3f, 0.0f, 1.0f,     1.0f, 1.0f,
             };
         }
 
@@ -82,6 +83,7 @@ namespace BasicTexture
 
             positionAttrib = GL.GetAttribLocation(shaderProgramID, "position");
             colorAttrib = GL.GetAttribLocation(shaderProgramID, "color");
+            textureAttrib = GL.GetAttribLocation(shaderProgramID, "texture");
 
         }
 
@@ -95,15 +97,40 @@ namespace BasicTexture
             GL.CompileShader(id);
             Console.WriteLine(GL.GetShaderInfoLog(id));
         }
-        
+
+        private void _LoadTexture()
+        {
+            textureID = GL.GenTexture();
+            GL.BindTexture(TextureTarget.Texture2D, textureID);
+
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+
+            Bitmap bmp = new Bitmap("faithful.png");
+            BitmapData bmpData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            GL.TexImage2D(TextureTarget.Texture2D, 
+                0, 
+                PixelInternalFormat.Rgba, 
+                bmpData.Width, 
+                bmpData.Height, 
+                0, 
+                OpenTK.Graphics.OpenGL.PixelFormat.Bgra, 
+                PixelType.UnsignedByte, 
+                bmpData.Scan0);
+             
+            bmp.UnlockBits(bmpData);
+        }
+
         private void _LoadBuffers()
         {
             GL.GenBuffers(1, out vertexVBO);
             GL.BindBuffer(BufferTarget.ArrayBuffer, vertexVBO);
 
             GL.BufferData(BufferTarget.ArrayBuffer, (IntPtr)(vertData.Length * sizeof(float)), vertData, BufferUsageHint.StaticDraw);
-            GL.VertexAttribPointer(positionAttrib, 3, VertexAttribPointerType.Float, false, sizeof(float) * 7, 0);
-            GL.VertexAttribPointer(colorAttrib, 4, VertexAttribPointerType.Float, false, sizeof(float) * 7, 3 * sizeof(float));            
+            GL.VertexAttribPointer(positionAttrib, 3, VertexAttribPointerType.Float, false, sizeof(float) * 9, 0);
+            GL.VertexAttribPointer(colorAttrib, 4, VertexAttribPointerType.Float, false, sizeof(float) * 9, 3 * sizeof(float));
+            GL.VertexAttribPointer(textureAttrib, 2, VertexAttribPointerType.Float, false, sizeof(float) * 9, 7 * sizeof(float));
 
             GL.UseProgram(shaderProgramID);
             GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
@@ -113,9 +140,11 @@ namespace BasicTexture
         {
             GL.EnableVertexAttribArray(positionAttrib);
             GL.EnableVertexAttribArray(colorAttrib);
+            GL.EnableVertexAttribArray(textureAttrib);
             GL.DrawArrays(PrimitiveType.Triangles, 0, 6);
             GL.DisableVertexAttribArray(positionAttrib);
             GL.DisableVertexAttribArray(colorAttrib);
+            GL.DisableVertexAttribArray(textureAttrib);
         }
     }
 }
--- BasicTexture/vs.glsl
+++ BasicTexture/vs.glsl
@@ -2,11 +2,14 @@
 
 in vec4 position;
 in vec4 color;
+in vec2 texture;
 
 out vec4 fragColor;
+out vec2 fragTexture;
 
 void main()
 {
 	gl_Position = position;
 	fragColor = color;
+	fragTexture = texture;
 }
