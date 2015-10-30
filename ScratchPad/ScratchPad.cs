5d20710b42e1973e67d9ad58a934504bc41979ab
remludar <jason.ewton@gmail.com>
2015-10-29 22:30:24 -0500

    ScratchPad Bitmap Loading with LAYERS!!!

--- ScratchPad/ScratchPad.cs
+++ ScratchPad/ScratchPad.cs
@@ -11,6 +11,7 @@ using System.IO;
 
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
 
 namespace ScratchPad
 {
@@ -19,14 +20,18 @@ namespace ScratchPad
         int program;
         int vao, vbo;
 
-        int textureID;
+        float[] vertexData;
+
+        int floorTexID, wallTexID;
         int width = 2;
         int height = 2;
-        int layerCount = 4;
-        int mipLevelCount = 2;
+        int layerCount = 2;
+        int mipLevelCount = 1;
         float[] texels;
+        byte[] texelBytes;
 
-        float[] vertexData;
+        Bitmap atlasBMP, floorBMP, wallBMP;
+        BitmapData atlasBMPData, floorBMPData, wallBMPData;
 
         protected override void OnLoad(EventArgs e)
         {
@@ -37,65 +42,10 @@ namespace ScratchPad
             GL.BindVertexArray(vao);
 
             GL.GenBuffers(1, out vbo);
-
-            vertexData = new float[]
-            {
-                0.0f, 0.0f,     0.0f, 1.0f, 0.0f,
-                1.0f, 0.0f,     1.0f, 1.0f, 0.0f,
-                1.0f, 1.0f,     1.0f, 0.0f, 0.0f,                                            
-                1.0f, 1.0f,     1.0f, 0.0f, 0.0f,
-                0.0f, 1.0f,     0.0f, 0.0f, 0.0f,
-                0.0f, 0.0f,     0.0f, 1.0f, 0.0f,
-
-                0.0f, -1.0f,    0.0f, 1.0f, 2.0f,
-                1.0f, -1.0f,    1.0f, 1.0f, 2.0f,
-                1.0f, 0.0f,     1.0f, 0.0f, 2.0f,                                            
-                1.0f, 0.0f,     1.0f, 0.0f, 2.0f,
-                0.0f, 0.0f,     0.0f, 0.0f, 2.0f,
-                0.0f, -1.0f,    0.0f, 1.0f, 2.0f,
-            };
-
-            //Read you texels here. In the current example, we have 2*2*2 = 8 texels, with each texel being 4 GLubytes.
-            texels = new float[]
-            {
-                 //Texels for first image.
-                 1.0f, 0.0f, 0.0f, 0.0f,
-                 0.0f, 0.0f, 0.0f, 0.0f,
-                 0.0f, 0.0f, 0.0f, 0.0f,
-                 0.0f, 0.0f, 0.0f, 0.0f,
-                 //Texels for second image.
-                 1.0f, 0.0f, 0.0f, 0.0f,
-                 1.0f, 0.0f, 0.0f, 0.0f,
-                 0.0f, 0.0f, 0.0f, 0.0f,
-                 0.0f, 0.0f, 0.0f, 0.0f,
-                 //Texels for third image.
-                 0.0f, 0.0f, 0.0f, 0.0f,
-                 1.0f, 0.0f, 0.0f, 0.0f,
-                 1.0f, 0.0f, 1.0f, 0.0f,
-                 0.0f, 0.0f, 0.0f, 0.0f,
-                 //Texels for fourth image.
-                 1.0f, 0.0f, 0.0f, 0.0f,
-                 1.0f, 0.0f, 0.0f, 0.0f,
-                 1.0f, 0.0f, 0.0f, 0.0f,
-                 1.0f, 0.0f, 0.0f, 0.0f,
-                 
-            };
-
-            GL.GenTextures(1, out textureID);
-            GL.BindTexture(TextureTarget.Texture2DArray, textureID);
-
-            //Allocate the storage.
-            //GL.TexStorage3D(TextureTarget3d.Texture2DArray, mipLevelCount, SizedInternalFormat.Rgba8, width, height, layerCount);
             
-
-            //Upload pixel data.
-            //The first 0 refers to the mipmap level (level 0, since there's only 1)
-            //The following 2 zeroes refers to the x and y offsets in case you only want to specify a subrectangle.
-            //The final 0 refers to the layer index offset (we start from index 0 and have 2 levels).
-            //Altogether you can specify a 3D box subset of the overall texture, but only one mip level at a time.
-            //GL.TexSubImage3D(TextureTarget.Texture2DArray, 0, 0, 0, 0, width, height, layerCount, OpenTK.Graphics.OpenGL.PixelFormat.Rgba, PixelType.Float, texels);
-            
-            //Always set reasonable texture parameters
+            _LoadVertexData();
+            //_LoadTexelData();
+            _LoadTexelDataFromBitmap();
 
             GL.TexParameter(TextureTarget.Texture2DArray, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
             GL.TexParameter(TextureTarget.Texture2DArray, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
@@ -118,27 +68,109 @@ namespace ScratchPad
             base.OnRenderFrame(e);
             GL.Clear(ClearBufferMask.ColorBufferBit);
 
-            //_DrawWithColor();
             _DrawWithTexture();
 
             GL.Flush();
             SwapBuffers();
         }
 
-        private void _DrawWithColor()
+
+        private void _LoadVertexData()
         {
-            GL.EnableVertexAttribArray(0);
+            vertexData = new float[]
+            {
 
-            GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
-            GL.BufferData(BufferTarget.ArrayBuffer, (IntPtr)(vertexData.Length * sizeof(float)), vertexData, BufferUsageHint.StaticDraw);
-            GL.VertexAttribPointer(0, 2, VertexAttribPointerType.Float, false, 0, 0);
-
-            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
-
-            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
-            GL.DisableVertexAttribArray(0);
+                -1.0f, +0.0f,   +0.0f, +1.0f, +0.0f,
+                +0.0f, +0.0f,   +1.0f, +1.0f, +0.0f,
+                +0.0f, +1.0f,   +1.0f, +0.0f, +0.0f,                                            
+                +0.0f, +1.0f,   +1.0f, +0.0f, +0.0f,
+                -1.0f, +1.0f,   +0.0f, +0.0f, +0.0f,
+                -1.0f, +0.0f,   +0.0f, +1.0f, +0.0f,
+                                              
+                +0.0f, +0.0f,   +0.0f, +1.0f, +1.0f,
+                +1.0f, +0.0f,   +1.0f, +1.0f, +1.0f,
+                +1.0f, +1.0f,   +1.0f, +0.0f, +1.0f,                                            
+                +1.0f, +1.0f,   +1.0f, +0.0f, +1.0f,
+                +0.0f, +1.0f,   +0.0f, +0.0f, +1.0f,
+                +0.0f, +0.0f,   +0.0f, +1.0f, +1.0f,
+                
+                -1.0f, -1.0f,   +0.0f, +1.0f, +1.0f,
+                +0.0f, -1.0f,   +1.0f, +1.0f, +1.0f,
+                +0.0f, +0.0f,   +1.0f, +0.0f, +1.0f,                                            
+                +0.0f, +0.0f,   +1.0f, +0.0f, +1.0f,
+                -1.0f, +0.0f,   +0.0f, +0.0f, +1.0f,
+                -1.0f, -1.0f,   +0.0f, +1.0f, +1.0f,
+              
+                +0.0f, -1.0f,   +0.0f, +1.0f, +0.0f,
+                +1.0f, -1.0f,   +1.0f, +1.0f, +0.0f,
+                +1.0f, +0.0f,   +1.0f, +0.0f, +0.0f,                                            
+                +1.0f, +0.0f,   +1.0f, +0.0f, +0.0f,
+                +0.0f, +0.0f,   +0.0f, +0.0f, +0.0f,
+                +0.0f, -1.0f,   +0.0f, +1.0f, +0.0f,
+            };
         }
+        private void _LoadTexelData()
+        {
+            //Read you texels here. In the current example, we have 2*2*2 = 8 texels, with each texel being 4 GLubytes.
+            texels = new float[]
+            {
+                 //Texels for first image.
+                 0.0f, 0.0f, 0.0f, 0.0f,
+                 0.0f, 0.0f, 0.0f, 0.0f,
+                 0.0f, 0.0f, 0.0f, 0.0f,
+                 1.0f, 0.0f, 0.0f, 0.0f,
+                 //Texels for second image.
+                 0.0f, 0.0f, 0.0f, 0.0f,
+                 0.0f, 0.0f, 0.0f, 0.0f,
+                 1.0f, 0.0f, 0.0f, 0.0f,
+                 0.0f, 0.0f, 0.0f, 0.0f,
+                 //Texels for third image.
+                 0.0f, 0.0f, 0.0f, 0.0f,
+                 1.0f, 0.0f, 0.0f, 0.0f,
+                 0.0f, 0.0f, 0.0f, 0.0f,
+                 0.0f, 0.0f, 0.0f, 0.0f,
+                 //Texels for fourth image.
+                 1.0f, 0.0f, 0.0f, 0.0f,
+                 0.0f, 0.0f, 0.0f, 0.0f,
+                 0.0f, 0.0f, 0.0f, 0.0f,
+                 0.0f, 0.0f, 0.0f, 0.0f,
+            };
 
+            GL.TexImage3D(TextureTarget.Texture2DArray, 0, PixelInternalFormat.Rgba8, width, height, layerCount, 0, OpenTK.Graphics.OpenGL.PixelFormat.Rgba, PixelType.Float, texels);
+        }
+        private void _LoadTexelDataFromBitmap()
+        {
+            //This code loads the whole bitmap
+            //Utilities.TextureLoader.LoadTextureAtlas(@"Content\Textures\SampleAtlas.png", out atlasBMP, out atlasBMPData);
+            //GL.TexImage3D(TextureTarget.Texture2DArray, 0, PixelInternalFormat.Rgba, atlasBMPData.Width, atlasBMPData.Height, 1, 0, OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, atlasBMPData.Scan0);
+
+            //Attempt to load 2 small bitmaps, convert to ubyte[], and render 
+            //Utilities.TextureLoader.LoadTexture(@"Content\Textures\floor.png", out floorBMP, out floorBMPData);
+            //GL.TexImage3D(TextureTarget.Texture2DArray, 0, PixelInternalFormat.Rgba8, floorBMPData.Width, floorBMPData.Height, 1, 0, OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, floorBMPData.Scan0);
+
+            //Utilities.TextureLoader.LoadTexture(@"Content\Textures\floor.png", out floorBMP, out floorBMPData);
+            //int bytes = floorBMPData.Stride * floorBMP.Height;
+            //byte[] floorBytes = new byte[bytes];
+            //Marshal.Copy(floorBMPData.Scan0, floorBytes, 0, bytes);
+            //GL.TexImage3D(TextureTarget.Texture2DArray, 0, PixelInternalFormat.Rgba8, floorBMPData.Width, floorBMPData.Height, layerCount, 0, OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, floorBytes);
+
+            Utilities.TextureLoader.LoadTexture(@"Content\Textures\floor.png", out floorBMP, out floorBMPData);
+            Utilities.TextureLoader.LoadTexture(@"Content\Textures\wall.jpg", out wallBMP, out wallBMPData);
+            int floorbytes = (floorBMPData.Stride * floorBMP.Height);
+            int wallbytes =(wallBMPData.Stride * wallBMP.Height);
+            int bytes = floorbytes + wallbytes;
+            byte[] texelBytes = new byte[floorbytes + wallbytes];
+            Marshal.Copy(floorBMPData.Scan0, texelBytes, 0, floorbytes); 
+            Marshal.Copy(wallBMPData.Scan0, texelBytes, floorbytes, wallbytes); 
+            int textureWidth = floorBMPData.Width;
+            int textureHeight = floorBMPData.Height;
+            
+
+            GL.TexImage3D(TextureTarget.Texture2DArray, 0, PixelInternalFormat.Rgba8, textureWidth, textureHeight, layerCount, 0, OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, texelBytes);
+
+
+
+        }
         private void _DrawWithTexture()
         {
             GL.EnableVertexAttribArray(0);
@@ -148,14 +180,13 @@ namespace ScratchPad
             GL.BufferData(BufferTarget.ArrayBuffer, (IntPtr)(vertexData.Length * sizeof(float)), vertexData, BufferUsageHint.StaticDraw);
             GL.VertexAttribPointer(0, 2, VertexAttribPointerType.Float, false, 5 * sizeof(float), 0);
             GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, 5 * sizeof(float), 2 * sizeof(float));
-            GL.TexImage3D(TextureTarget.Texture2DArray, 0 , PixelInternalFormat.Rgba8, width, height, layerCount, 0, OpenTK.Graphics.OpenGL.PixelFormat.Rgba, PixelType.Float, texels);
-            GL.DrawArrays(PrimitiveType.Triangles, 0, 12);
+
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 24);
 
             GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
             GL.DisableVertexAttribArray(0);
             GL.DisableVertexAttribArray(1);
         }
-
         private void _ProcessInput()
         {
             var keyboardState = OpenTK.Input.Keyboard.GetState();
--- ScratchPad/Utility.cs
+++ ScratchPad/Utility.cs
@@ -69,16 +69,18 @@ namespace Utilities
             bmp.UnlockBits(bmpData);
         }
 
-        public static void LoadTextureAtlas(out int atlasID, string filePath, out Bitmap bmp, out BitmapData bmpData)
+        public static void LoadTextureAtlas(string filePath, out Bitmap bmp, out BitmapData bmpData)
         {
-            atlasID = GL.GenTexture();
-            GL.BindTexture(TextureTarget.Texture2D, atlasID);
-
-            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
-            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
-
             bmp = new Bitmap(filePath);
             bmpData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+        }
+
+        public static void LoadTexture(string filePath, out Bitmap bmp, out BitmapData bmpData)
+        {
+            bmp = new Bitmap(filePath);
+            bmpData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
         }
     }
 }
